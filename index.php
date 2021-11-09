ff361e6f9e395073b191fce03cf7a8efa1c32b9d
BestUserNameYouWillSee <77840705+BUNYWS@users.noreply.github.com>
2021-11-09 12:33:15 +0100

    footer styling
    styling footer + footer add

---
+++ index.php
@@ -0,0 +1,35 @@
+<DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/styling.css">
+    </head>
+    <body>
+
+
+        <div class="grid-container">
+            <div class="grid-item">
+                Contact us
+                <div class="grid-item-2">
+                    0612345678 <br/>
+                    examplecontact@gmail.com<br/>
+                    Vista College<br/>
+                    Adres plaats<br/>
+                    postcode<br/>
+                    land
+                </div>
+            </div>
+            <div class="grid-item">
+                Menu
+                <div class="grid-item-2">
+                    
+                </div>
+            </div>
+            <div class="grid-item">
+                About us
+                <div class="grid-item-2">
+                    
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
