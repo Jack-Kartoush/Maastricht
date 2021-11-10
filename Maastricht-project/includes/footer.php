60f2c8cd26d80eacd766d4d3302f2c385efdd58f
Jack Kartosh <jackkartosh@gmail.com>
2021-11-10 10:25:52 +0100

    index + footer + de styling
    Here werd de index en de footer + de styling daar van

---
+++ Maastricht-project/includes/footer.php
@@ -0,0 +1,49 @@
+<section class="footer-container">
+        <div class="footer-innercontainer">
+            <nav class="footer-nav">
+                <ul class="footer-ul">
+                    <div class="About">
+                        <li>
+                            <h3 class='footer-title'>Maastricht Geschidenis</h3>
+                        </li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Over de Bestuur</a></li>
+                        <li><a href="#">Team</a></li>
+
+                    </div>
+                    <div class="snel-Link">
+                        <li>
+                            <h3 class='footer-title'>Contact</h3>
+                        </li>
+                        <li><a href="#">tel: 0612345678</a></li>
+                        <li><a href="#"> e-mail: examplecontact@gmail.com</a></li>
+                        <li><a href="#">plaats: Limburig</a></li>
+                        <li><a href="#"> postcode: 6584 DT</a></li>
+                        <li><a href="#">land: Nederland</a></li>
+
+                    </div>
+                    <div class="Overige">
+                        <li>
+                            <h3 class='footer-title'>Geschidenis Over</h3>
+                        </li>
+                        <li><a href="#">1500-1700</a></li>
+                        <li><a href="#">De kastelen</a></li>
+                        <li><a href="#">1900-1988</a></li>
+                    </div>
+                </ul>
+            </nav>
+        </div>
+        <div class='voorwaarden'>
+            <div class="inner-voorwaarden">
+                <a class='footer-item' href='#' id='voorwarden-items'>Algemeen voorwaarden</a>
+                <a class='footer-item'href='#' id='voorwarden-items'>Privacybeleid</a>
+                <a class='footer-item' href='#' id='voorwarden-items'>Cookiebeleid</a>
+
+            </div>
+           
+        </div>
+        <h5 id='footer-subtitle'>Maastricht <small>&COPY 2021</small> </h5>
+        
+</section>
+</body>
+</html>
---
+++ Maastricht-project/index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./style/Style.css">
+    <title>Maastricht Geschidenis</title>
+</head>
+<body>
+    <?php include './includes/menu.php';?>
+    <section class="template-holder">
+        <div class="template-container">
+
+            <div class="templats">
+            <div class="innertemplat-foto">
+                <img src="./img/Attak.jpeg" alt="Eroro-Foto niet gevonden" id="Fotos-ID"> 
+            </div>
+               
+            <p> Why do we use it? It is a long 
+            established fact that a reader will 
+            be distracted by the readable 
+            content of a page when looking at its
+            layout. The point of using Lorem Ipsum</p>
+            <br>
+            <a href="#">Meer Lezen</a>
+               
+            </div>
+            <div class="templats">
+
+            <div class="innertemplat-foto">
+                <img src="./img/Aanval.jpeg" alt="Eroro-Foto niet gevonden" id="Fotos-ID"> 
+            </div>
+               
+            <p>Why do we use it? It is a long 
+            established fact that a reader will 
+            be distracted by the readable 
+            content of a page when looking at its
+            layout. The point of using Lorem Ipsum 
+            Why do we use it? It is a long 
+            established fact that a reader will 
+            be distracted by the readable 
+            content of a page when looking at its
+            layout. The point of using Lorem Ipsum 
+           </p>
+            <br>
+            <a href="#">Meer Lezen</a>
+
+            </div>
+        </div>
+          <!-- begining with the 2 templats -->
+        <div class="template-container">
+            
+            <div class="templats-long">
+            <div class="innertemplat-foto">
+                <img src="./img/cannon.jpeg" alt="Eroro-Foto niet gevonden" id="Fotos-ID"> 
+            </div>
+               
+            <p> Why do we use it? It is a long 
+            established fact that a reader will 
+            be distracted by the readable 
+            content of a page when looking at its
+            layout. The point of using Lorem Ipsum
+            Why do we use it? It is a long 
+            established fact that a reader will 
+            be distracted by the readable 
+            content of a page when looking at its
+            layout. The point of using Lorem Ipsum</p>
+            <br>
+            <a href="#">Meer Lezen</a>
+               
+            </div>
+            <div class="templats">
+
+            <div class="innertemplat-foto">
+                <img src="./img/overheid.jpeg" alt="Eroro-Foto niet gevonden" id="Fotos-ID"> 
+            </div>
+               
+            <p>Why do we use it? It is a long 
+            established fact that a reader will 
+            be distracted by the readable 
+            content of a page when looking at its
+            layout. The point of using Lorem Ipsum 
+            Why do we use it? It is a long 
+            established fact that a reader will 
+            be distracted by the readable 
+            content of a page when looking at its
+            layout. The point of using Lorem Ipsum </p>
+            Why do we use it? It is a long 
+            established fact that a reader will 
+            be distracted by the readable 
+            content of a page when looking at its
+            layout. The point of using Lorem Ipsum 
+            Why do we use it? It is a long 
+            established fact that a reader will 
+            be distracted by the readable 
+            content of a page when looking at its
+            layout. The point of using Lorem Ipsum 
+            <br>
+            <a href="#">Meer Lezen</a>
+
+            </div>
+               <div class="templats">
+
+            <div class="innertemplat-foto">
+                <img src="./img/bescherming-binnen-kasteel.jpeg" alt="Eroro-Foto niet gevonden" id="Fotos-ID"> 
+            </div>
+               
+            <p>Why do we use it? It is a long 
+            established fact that a reader will 
+            be distracted by the readable 
+            content of a page when looking at its
+            layout. The point of using Lorem Ipsum
+            Why do we use it? It is a long 
+            established fact that a reader will 
+            be distracted by the readable 
+            content of a page when looking at its
+            layout. The point of using Lorem Ipsum </p>
+            
+            <br>
+            <a href="#">Meer Lezen</a>
+
+            </div>
+               <div class="templats-long">
+
+            <div class="innertemplat-foto">
+                <img src="./img/kasteel.jpeg" alt="Eroro-Foto niet gevonden" id="Fotos-ID" target="_blank"> 
+            </div>
+               
+            <p>Why do we use it? It is a long 
+            established fact that a reader will 
+            be distracted by the readable 
+            content of a page when looking at its
+            layout. The point of using Lorem Ipsum 
+            Why do we use it? It is a long 
+            established fact that a reader will 
+            be distracted by the readable 
+            content of a page when looking at its
+            layout. The point of using Lorem Ipsum
+        
+        </p>
+            <br>
+            <a href="#">Meer Lezen</a>
+
+            </div>
+            
+        </div>
+            <!-- begining with the 3 templats -->
+            <div class="template-container">
+
+            <div class="templats-3">
+            <div class="innertemplat-foto">
+                <img src="./img/R-kasteel.jpeg" alt="Eroro-Foto niet gevonden" id="Fotos-ID" > 
+            </div>
+               
+            <p> Why do we use it? It is a long 
+            established fact that a reader will 
+            be distracted by the readable 
+            content of a page when looking at its
+            layout. The point of using Lorem Ipsum
+            Why do we use it? It is a long 
+            established fact that a reader will 
+            be distracted by the readable 
+            content of a page when looking at its
+            layout. The point of using Lorem Ipsum</p>
+            <br>
+            <a href="#">Meer Lezen</a>
+               
+            </div>
+            <div class="templats-3">
+
+            <div class="innertemplat-foto">
+                <img src="./img/Aanval.jpeg" alt="Eroro-Foto niet gevonden" id="Fotos-ID"> 
+            </div>
+               
+            <p>Why do we use it? It is a long 
+            established fact that a reader will 
+            be distracted by the readable 
+            content of a page when looking at its
+            layout. The point of using Lorem Ipsum
+            Why do we use it? It is a long 
+            established fact that a reader will 
+            be distracted by the readable 
+            content of a page when looking at its
+            layout. The point of using Lorem Ipsum </p>
+            <br>
+            <a href="#">Meer Lezen</a>
+            </div>
+        </div>
+        <?php include './includes/footer.php';?> 
+    </section> 
+
+           
+
+
+ 
