024c4ef6f235af00449af5ef0f1f8274b9c9cc52
Jack-Kartoush <75987684+Jack-Kartoush@users.noreply.github.com>
2022-03-29 14:34:37 +0200

    Add files via upload

--- Maastricht-project/includes/menu.php
+++ Maastricht-project/includes/menu.php
@@ -1,14 +1,16 @@
 <div class="header-container">
-<div class="logo">
-   <h1> <a href="./index.php">Maastricht Geschidenis</a></h1>
-</div>
-<header>
-    <nav>
-        <ul>
-            <li class="item active"><a href="./index.php">Home</a></li>
-            <li class="item"><a href="./About.php">About us</a></li>
-            <li class="item"><a href="#contact">Contact us</a></li>
-        </ul>
-    </nav>
-</header>
+    <div class="logo-container">
+        <div class="logo">
+            <h1> <a href="./index.php"><span>Maastricht</span> Geschiedenis</a></h1>
+        </div>
+    </div>
+    <header>
+        <nav>
+            <ul>
+                <li class="item active"><a href="./index.php">Home</a></li>
+                <li class="item"><a href="./About.php">About us</a></li>
+                <li class="item"><a href="#contact">Contact us</a></li>
+            </ul>
+        </nav>
+    </header>
 </div>
